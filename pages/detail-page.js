486cc0466f2e1dd57d58000aeaac31c433b215da
nirustanite <nirustanite@gmail.com>
2019-08-28 17:13:35 +0200

    Added Validations

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,9 +1,32 @@
+
+function doesNotPassAllValidations(name, msg){
+    if(!name || !msg){
+        alert("You haven't entered a name or message");
+        return true;
+    }
+
+    if(msg.length > 200){
+        alert("Your comment is too long")
+    }
+
+    return false;
+}
+
+
+
+
+
 function submitComment()
 {
     const inputField = document.getElementById('name');
     const name = inputField.value;
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
+    
+    if(doesNotPassAllValidations(name,msg)){
+        return null;
+    }
+    
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
